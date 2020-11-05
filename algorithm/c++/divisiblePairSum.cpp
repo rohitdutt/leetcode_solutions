e8e7c7c86154144fa856f598c75e8136fa55960e
Rohitdutt <parsairohit2@gmail.com>
2020-11-05 13:54:32 +0530

    divisible pair sum

---
+++ algorithm/c++/divisiblePairSum.cpp
@@ -0,0 +1,17 @@
+#include<iostream>;
+#include<vector>;
+using namespace std;
+
+class Solution{
+public: 
+    int divisiblePairSum(vector<int> v1 , int k){
+        for()
+        return 0;
+    }
+};
+ 
+int main() {
+    Solution s;
+    s.divisiblePairSum();
+    return 0;
+}
