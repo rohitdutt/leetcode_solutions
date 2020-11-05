b4c5db7f96719ae5df277077d4cb67b3fb5dba01
Rohitdutt <parsairohit2@gmail.com>
2020-11-05 18:55:47 +0530

    divisable pair sum

--- algorithm/c++/divisiblePairSum.cpp
+++ algorithm/c++/divisiblePairSum.cpp
@@ -1,17 +1,31 @@
-#include<iostream>;
-#include<vector>;
+#include<iostream>
+#include<vector>
 using namespace std;
 
 class Solution{
+    int res=0;
 public: 
     int divisiblePairSum(vector<int> v1 , int k){
-        for()
-        return 0;
+        for(int i: v1){
+            for(int j: v1){
+                if(!((i+j)%k)&&(i<j)){
+                    res++;
+                }
+            }
+        }
+        return res;
     }
 };
  
 int main() {
     Solution s;
-    s.divisiblePairSum();
+    vector<int> v;
+    v.push_back(1);
+    v.push_back(3);
+    v.push_back(2);
+    v.push_back(6);
+    v.push_back(1);
+    v.push_back(2);
+    cout<<s.divisiblePairSum(v,6);
     return 0;
 }
