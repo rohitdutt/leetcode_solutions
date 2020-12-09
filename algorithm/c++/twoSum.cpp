2803bed02c239b778f4fef270c03c8cc1c179599
Rohitdutt <parsairohit2@gmail.com>
2020-12-09 11:37:07 +0530

    optimized two sum

--- algorithm/c++/twoSum.cpp
+++ algorithm/c++/twoSum.cpp
@@ -1,35 +1,43 @@
-#include<iostream>
-#include<vector>
-#include<unordered_map>
+#include <iostream>
+#include <vector>
+#include <unordered_map>
 
 using namespace std;
 
-class Solution {
-    unordered_map<int,int> um1;
-    int temp;
+class Solution
+{
+    unordered_map<int, int> um1;
     vector<int> answer;
+
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        for(int i=0;i<nums.size();i++){
-            if(um1[nums.at(i)]==nums.at(i)){
+    vector<int> twoSum(vector<int> &nums, int target)
+    {
+        for (int i = 0; i < nums.size(); i++)
+        {
+            um1[nums[i]] = i;
+        }
+        for (int i = 0; i < nums.size(); i++)
+        {
+            int temp = target - nums[i];
+            if (um1[temp])
+            {
+                answer.push_back(um1[temp]);
                 answer.push_back(i);
-            }else{
-                temp=target-nums.at(i);
-                cout<<"temp"<<temp<<endl;
-                um1[nums.at(i)]=i;
-                cout<<um1[nums.at(i)];
+                return answer;
             }
-        } 
-        for(int i:answer){
-            cout<<i;
-        }             
+        }
         return answer;
     }
 };
 
-int main(){
+int main()
+{
     Solution s1;
-    vector<int> values={2,3,5,6,7};
-    values=s1.twoSum(values,9);
-
+    vector<int> values = {1, 3, 4, 2};
+    vector<int> answer;
+    answer = s1.twoSum(values, 6);
+    for (int i : answer)
+    {
+        cout << i << endl;
+    }
 }
